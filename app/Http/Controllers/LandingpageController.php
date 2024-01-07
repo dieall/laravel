44888a9e1741220beaececf2e665208cd7ee1b33
AA_152022253_Aldi <aldi@mhs.itenas.ac.id>
2024-01-07 15:47:23 +0700

    update landing page

--- app/Http/Controllers/LandingpageController.php
+++ app/Http/Controllers/LandingpageController.php
@@ -12,42 +12,8 @@ class LandingpageController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
-        return view('landingpage', compact('products'));
+        return view('landingpage');
     }
-
-    public function beli()
-    {
-        $products = Product::all();
-        $pelanggans = Pelanggan::all();
-        $categories = Kategori::all();
     
-        return view('landingpage.beli', compact('products', 'pelanggans', 'categories'));
-    }
 
-    public function store(Request $request)
-    {
-        // Validasi request jika diperlukan
-        $validatedData = $request->validate([
-            'tgl_beli' => 'required|date',
-            'id_barang' => 'required',
-            'kategori_id' => 'required',
-            'id_pelanggan' => 'required',
-        ]);
-
-        // Simpan data ke dalam database
-        $transaksi = new Transaksi([
-            'tgl_beli' => $validatedData['tgl_beli'],
-            'id_barang' => $validatedData['id_barang'],
-            'kategori_id' => $validatedData['kategori_id'],
-            'id_pelanggan' => $validatedData['id_pelanggan'],
-            'keterangan' => $request->input('keterangan'), // Kolom tambahan, sesuaikan dengan kebutuhan
-        ]);
-
-        // Simpan transaksi ke dalam database
-        $transaksi->save();
-
-        // Redirect ke halaman landing page atau halaman lain yang sesuai
-        return redirect()->route('landingpage.index')->with('success', 'Transaksi berhasil disimpan.');
-    }
 }
--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,146 +1,242 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('layout/bootstrap/dist/css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('layout/css/main.css') }}">
-<title>Toko Sejahtera</title>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+<meta name="description" content=""/>
+<meta name="author" content=""/>
+<title>New Age - Start Bootstrap Theme</title>
+<link rel="icon" type="image/x-icon" href="{{ asset('landing/assets/favicon.ico') }}"/>
+<!-- Bootstrap icons-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
+<!-- Google fonts-->
+<link rel="preconnect" href="https://fonts.gstatic.com"/>
+<link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet"/>
+<!-- Core theme CSS (includes Bootstrap)-->
+<link href="{{ asset('landing/css/styles.css') }}" rel="stylesheet"/>
 </head>
-<body>
-<nav class="navbar navbar-expand-lg bg-white fixed-top py-4 shadow-sm">
-<div class="container">
-	<a class="navbar-brand" href="index.html">Toko Sejahtera</a>
-	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	<span class="navbar-toggler-icon"></span>
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+<div class="container px-5">
+	<a class="navbar-brand fw-bold" href="#page-top">Toko Sejahtera</a>
+	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	Menu <i class="bi-list"></i>
 	</button>
-	<div class="collapse navbar-collapse" id="navbarNav">
-		<div class="input-group mx-auto mt-5 mt-lg-0">
-			<input type="text" class="form-control" placeholder="Mau cari apa?" aria-label="Mau cari apa?" aria-describedby="button-addon2">
-			<button class="btn btn-outline-warning" type="button" id="button-addon2"><i class="bx bx-search"></i></button>
-		</div>
-		<ul class="navbar-nav ms-auto mt-3 mt-sm-0">
-
-
-			@if (Route::has('login'))
-			<li class="nav-item mt-5 mt-lg-0 text-center">
-				<a class="nav-link btn-second me-lg-3" href="{{ route('login') }}">Login</a>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+			<li class="nav-item">
+				<a class="nav-link me-lg-3" href="#features">Features</a>
 			</li>
-			<li class="nav-item mt-3 mt-lg-0 text-center">
-				<a class="nav-link btn-first" href="{{ route('register') }}">Register</a>
+			<li class="nav-item">
+				<a class="nav-link me-lg-3" href="#download">Download</a>
 			</li>
-            @endif
 		</ul>
+		 @if (Route::has('login')) <button class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
+		<span class="d-flex align-items-center">
+		<i class="bi-chat-text-fill me-2"></i>
+		<a href="{{ route('login') }}" class="text-white">Login</a>
+		</span>
+		</button>
+		@endif
 	</div>
 </div>
 </nav>
-
-<!-- Header -->
-<section class="header">
-<div class="container">
-	<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-		<div class="carousel-indicators">
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-		</div>
-		<div class="carousel-inner">
-			<div class="carousel-item active">
-				<img src="{{ asset('layout/assets/img/slide1.jpg') }}" class="d-block w-100" alt="slide 1">
-			</div>
-			<div class="carousel-item">
-				<img src="{{ asset('layout/assets/img/slide2.jpg') }}" class="d-block w-100" alt="slide 2">
+<!-- Mashead header-->
+<header class="masthead">
+<div class="container px-5">
+	<div class="row gx-5 align-items-center">
+		<div class="col-lg-6">
+			<!-- Mashead text and app badges-->
+			<div class="mb-5 mb-lg-0 text-center text-lg-start">
+				<h1 class="display-1 lh-1 mb-3">Toko Sejahtera</h1>
+				<p class="lead fw-normal text-muted mb-5">
+					Aplikasi untuk mencatat segala transaksi di Toko Sejahtera
+				</p>
 			</div>
 		</div>
-		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		<span class="visually-hidden">Previous</span>
-		</button>
-		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
-		<span class="visually-hidden">Next</span>
-		</button>
+		<div class="col-lg-6">
+			<!-- Masthead device mockup feature-->
+			<div class="masthead-device-mockup">
+				<svg class="circle" viewbox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+				<defs>
+				<lineargradient id="circleGradient" gradienttransform="rotate(45)">
+				<stop class="gradient-start-color" offset="0%"></stop>
+				<stop class="gradient-end-color" offset="100%"></stop>
+				</lineargradient>
+				</defs>
+				<circle cx="50" cy="50" r="50"></circle></svg><svg class="shape-1 d-none d-sm-block" viewbox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
+				<rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
+				<rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect></svg><svg class="shape-2 d-none d-sm-block" viewbox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50"></circle></svg>
+				<div class="device-wrapper">
+					<div class="device" data-device="iPhoneX" data-orientation="portrait" data-color="black">
+						<div class="screen bg-black">
+							<!-- PUT CONTENTS HERE:-->
+							<!-- * * This can be a video, image, or just about anything else.-->
+							<!-- * * Set the max width of your media to 100% and the height to-->
+							<!-- * * 100% like the demo example below.-->
+							<video muted="muted" autoplay="" loop="" style="max-width: 100%; height: 100%"><source src="{{ asset('landing/assets/img/demo-screen.mp4') }}" type="video/mp4"/></video>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</header>
+<!-- Quote/testimonial aside-->
+<aside class="text-center bg-gradient-primary-to-secondary">
+<div class="container px-5">
+	<div class="row gx-5 justify-content-center">
+		<div class="col-xl-8">
+			<div class="h2 fs-1 text-white mb-4">
+				"Bekerjalah dengan jujur maka anda akan mendapatkan berkah"
+			</div>
+			<img src="{{ asset('landing/assets/img/tnw-logo.svg') }}" alt="..." style="height: 3rem"/>
+		</div>
+	</div>
+</div>
+</aside>
+<!-- App features section-->
+<section id="features">
+<div class="container px-5">
+	<div class="row gx-5 align-items-center">
+		<div class="col-lg-8 order-lg-1 mb-5 mb-lg-0">
+			<div class="container-fluid px-5">
+				<div class="row gx-5">
+					<div class="col-md-6 mb-5">
+						<!-- Feature item-->
+						<div class="text-center">
+							<i class="bi-phone icon-feature text-gradient d-block mb-3"></i>
+							<h3 class="font-alt">Device Mockups</h3>
+							<p class="text-muted mb-0">
+								Tampilan Mockup
+							</p>
+						</div>
+					</div>
+					<div class="col-md-6 mb-5">
+						<!-- Feature item-->
+						<div class="text-center">
+							<i class="bi-camera icon-feature text-gradient d-block mb-3"></i>
+							<h3 class="font-alt">Pemakaian</h3>
+							<p class="text-muted mb-0">
+								Pemakaian yang mudah dan praktis
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6 mb-5 mb-md-0">
+						<!-- Feature item-->
+						<div class="text-center">
+							<i class="bi-gift icon-feature text-gradient d-block mb-3"></i>
+							<h3 class="font-alt">Terpercaya</h3>
+							<p class="text-muted mb-0">
+								Sangat dipercayai oleh beberapa pihak
+							</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<!-- Feature item-->
+						<div class="text-center">
+							<i class="bi-patch-check icon-feature text-gradient d-block mb-3"></i>
+							<h3 class="font-alt">Halal</h3>
+							<p class="text-muted mb-0">
+								Dijamin Halal
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 order-lg-0">
+			<!-- Features section device mockup-->
+			<div class="features-device-mockup">
+				<svg class="circle" viewbox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+				<defs>
+				<lineargradient id="circleGradient" gradienttransform="rotate(45)">
+				<stop class="gradient-start-color" offset="0%"></stop>
+				<stop class="gradient-end-color" offset="100%"></stop>
+				</lineargradient>
+				</defs>
+				<circle cx="50" cy="50" r="50"></circle></svg><svg class="shape-1 d-none d-sm-block" viewbox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
+				<rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
+				<rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect></svg><svg class="shape-2 d-none d-sm-block" viewbox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50"></circle></svg>
+				<div class="device-wrapper">
+					<div class="device" data-device="iPhoneX" data-orientation="portrait" data-color="black">
+						<div class="screen bg-black">
+							<!-- PUT CONTENTS HERE:-->
+							<!-- * * This can be a video, image, or just about anything else.-->
+							<!-- * * Set the max width of your media to 100% and the height to-->
+							<!-- * * 100% like the demo example below.-->
+							<video muted="muted" autoplay="" loop="" style="max-width: 100%; height: 100%"><source src="{{ asset('landing/assets/img/demo-screen.mp4') }}" type="video/mp4"/></video>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 </section>
-<!-- Popular -->
-<section class="popular">
-    <div class="container">
-        <!-- ... (bagian lain dari kode) ... -->
-
-        <div class="row mt-5">
-            @foreach ($products as $product)
-                <div class="col-lg-3 col-6">
-                    <div class="card card-product card-body p-lg-4 p3">
-                        <a href="{{ route('product.show', $product->id_barang) }}">
-                            <img src="{{ asset('uploads/' . $product->image) }}" alt="{{ $product->name }}" class="img-fluid">
-                        </a>
-                        <h3 class="product-name mt-3">{{ $product->nama }}</h3>
-                        <!-- Tambahkan informasi produk lainnya sesuai kebutuhan -->
-                        <div class="detail d-flex justify-content-between align-items-center mt-4">
-                            <p class="price">IDR {{ number_format($product->harga, 2) }}</p>
-                            <a href="{{ route('landingpage.beli') }}" class="btn-cart"><i class="bx bx-cart-alt"></i></a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-</section>
-
-<!-- Subscribe  -->
-
-<footer>
-<div class="container pt-5">
-	<div class="row row-content">
-		<div class="col-md-6">
-			<h1 class="logo-brand">Indo <span>Toko</span></h1>
-			<p>
-				Lorem ipsum dolor sit amet
+<!-- Basic features section-->
+<section class="bg-light">
+<div class="container px-5">
+	<div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+		<div class="col-12 col-lg-5">
+			<h2 class="display-4 lh-1 mb-4">Toko Sejahtera</h2>
+			<p class="lead fw-normal text-muted mb-5 mb-lg-0">
+				Toko sejahtera ini dibangun untuk memenuhi tugas besar Ujian Akhir Semester mata kuliah Pemrograman Web
 			</p>
 		</div>
-		<div class="col-md-3 mt-4 mt-sm-0">
-			<h3 class="mb-3">Navigation</h3>
-			<ul class="p-0">
-				<li>
-					<a href="#">Home</a>
-				</li>
-				<li class="mt-3">
-					<a href="#">Best Seller</a>
-				</li>
-				<li class="mt-3">
-					<a href="#">Category</a>
-				</li>
-				<li class="mt-3">
-					<a href="#">Comunity</a>
-				</li>
-				<li class="mt-3">
-					<a href="#">Blog</a>
-				</li>
-			</ul>
-		</div>
-		<div class="col-md-3 mt-4 mt-sm-0">
-			<h3 class="mb-3">Company</h3>
-			<a href="#">john@example.com</a>
-			<p>
-				Jln. Tamansiswa, No 32 Yogyakarta Indonesia
-			</p>
+		<div class="col-sm-8 col-md-6">
+			<div class="px-5 px-sm-0">
+				<img class="img-fluid rounded-circle" src="https://source.unsplash.com/u8Jn2rzYIps/900x900" alt="..."/>
+			</div>
 		</div>
 	</div>
-	<div class="row row-copy mt-4 mt-sm-0">
-		<div class="col-md-6">
-			<p>
-				&copy; 2023 IndoToko. All rights reserved.
-			</p>
-		</div>
-		<div class="col-md-6 text-sm-end">
-			<a href="#"><i class='bx bxl-instagram-alt'></i></a>
-			<a href="#"><i class='bx bxl-facebook-circle'></i></a>
-		</div>
+</div>
+</section>
+<!-- Call to action section-->
+<section class="cta">
+<div class="cta-content">
+	<div class="container px-5">
+		<h2 class="text-white display-1 lh-1 mb-4">Jangan Kebanyakan Berfikir.<br/>Mari Bekerja ! ! !</h2>
+	</div>
+</div>
+</section>
+<!-- App badge section-->
+<section class="bg-gradient-primary-to-secondary" id="download">
+<div class="container px-5">
+	<h2 class="text-center text-white font-alt mb-4">Get the app now!</h2>
+	<div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
+		<a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src="{{ asset('landing/assets/img/google-play-badge.svg') }}" alt="..."/></a>
+		<a href="#!"><img class="app-badge" src="{{ asset('landing/assets/img/app-store-badge.svg') }}" alt="..."/></a>
+	</div>
+</div>
+</section>
+<!-- Footer-->
+<footer class="bg-black text-center py-5">
+<div class="container px-5">
+	<div class="text-white-50 small">
+		<div class="mb-2">&copy;Toko Sejahtera 2024. All Rights Reserved.</div>
+		<a href="#!">Privacy</a>
+		<span class="mx-1">&middot;</span>
+		<a href="#!">Terms</a>
+		<span class="mx-1">&middot;</span>
+		<a href="#!">FAQ</a>
 	</div>
 </div>
 </footer>
-<script src="{{ asset('layout/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="{{ asset('landing/js/scripts.js') }}"></script>
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<!-- * *                               SB Forms JS                               * *-->
+<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
 </html>
